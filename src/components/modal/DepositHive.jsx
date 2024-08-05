8b392845094260f0e23d86a0fc4231415a39be0a
Adesojisouljay <adesojiisrael@gmail.com>
2024-08-05 12:17:38 +0100

    fixed memo and modal logics

--- src/components/modal/DepositHive.jsx
+++ src/components/modal/DepositHive.jsx
@@ -1,14 +1,17 @@
-import './deposit-modal.scss';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import './deposit-modal.scss';
 
-export const DepositHiveModal = ({ isOpen, onClose }) => {
+export const DepositHiveModal = ({ isOpen, onClose, assets }) => {
 
   const user = useSelector(state => state.apexMiner.user);
-  console.log(user)
+
+  const [selectedAsset, setSelectedAsset] = useState(assets[0]);
+  // console.log(user)
 
   const handleCopyAddress = () => {
-    navigator.clipboard.writeText("souljay");
+    navigator.clipboard.writeText(selectedAsset.depositAddress);
     toast.success("Address copied to clipboarc!",{
     style: {
       backgroundColor: 'rgba(229, 229, 229, 0.1)',
@@ -18,7 +21,7 @@ export const DepositHiveModal = ({ isOpen, onClose }) => {
   });
   };
   const handleCopyMemo = () => {
-    navigator.clipboard.writeText(user?._id);
+    navigator.clipboard.writeText(selectedAsset.memo);
     toast.success("Memo copied to clipboarc!", {
       style: {
         backgroundColor: 'rgba(229, 229, 229, 0.1)',
@@ -28,23 +31,36 @@ export const DepositHiveModal = ({ isOpen, onClose }) => {
     });
   };
 
+  const handleAssetChange = (e) => {
+    const asset = assets.find(asset => asset.currency === e.target.value);
+    setSelectedAsset(asset);
+  };
+
   return (
     <div className={`fadded-container modal-overlay ${isOpen ? 'open' : ''}`} >
     <div className={`modal-overlay  ${isOpen ? 'open' : ''}`} onClick={onClose}> </div>
       <div className="modal animate-slide-in  animate-slide-in-mobile">
         <span className="close-btn" onClick={onClose}>X</span>
-        <h2>Deposit</h2> 
-          <div className="deposit-address">
-            <span>Deposit Address:</span>
-            <span>souljay</span>
-            {<button className="generate-address-btn" onClick={handleCopyAddress}>Copy Address</button>}
-          </div>
-          <div className="deposit-address">
-            <span>Deposit Memo</span>
-            <span className='warning'>(please make sure you copy your memo correctly)</span>
-            <span>{user?._id}</span>
-            {<button className="generate-address-btn" onClick={handleCopyMemo}>Copy Address</button>}
-          </div>
+        <h2>Deposit</h2>
+        <div className="deposit-asset">
+          <label htmlFor="asset-select">Select Asset:</label>
+          <select id="asset-select" onChange={handleAssetChange} value={selectedAsset.currency}>
+            {assets.map(asset => (
+              <option key={asset.currency} value={asset.currency}>{asset.currency.toUpperCase()}</option>
+            ))}
+          </select>
+        </div>
+        <div className="deposit-address">
+          <span>Deposit Address:</span>
+          <span>{selectedAsset.depositAddress}</span>
+          {<button className="generate-address-btn" onClick={handleCopyAddress}>Copy Address</button>}
+        </div>
+        <div className="deposit-address">
+          <span>Deposit Memo</span>
+          <span className='warning'>(please make sure you copy your memo correctly)</span>
+          <span>{selectedAsset.memo}</span>
+          {<button className="generate-address-btn" onClick={handleCopyMemo}>Copy Address</button>}
+        </div>
       </div>
     
     </div>
