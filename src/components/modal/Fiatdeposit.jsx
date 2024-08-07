1b40e65d6ff88d01f2e3e63a16adfcc6a4479a60
Adesojisouljay <adesojiisrael@gmail.com>
2024-08-07 12:49:40 +0100

    get merchant and create deposit request

--- src/components/modal/Fiatdeposit.jsx
+++ src/components/modal/Fiatdeposit.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./fiat-deposit.scss"
 import { IoIosCopy } from "react-icons/io";
 import { FaLessThan } from "react-icons/fa6";
 import { useState } from 'react';
-import { useEffect } from 'react';
+import { getRandomMerchant, createNairaDepositRequest } from '../../api/ekzat';
 
 function Fiatdeposit({ isOpen, onClose}) {
   const [depositAmout, setDepositAmount] = useState("")
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(1);
+  const [narration, SetNarration] = useState("");
+  const [merchantInfo, setMerchantInfo] = useState({})
 
-  const nextStep = () =>{
-    if(depositAmout => 1000 ){
-      setStep(2)
+  useEffect(() => {
+    if (step === 2) {
+      console.log("Merchant Info:", merchantInfo);
+    }
+  }, [step, merchantInfo]);
+
+  const getMerchant = async () =>{
+      const merchantData = await getRandomMerchant()
+    try {     
+      if(!depositAmout || Number(depositAmout) < 1000){
+        return;
+      } else {
+        setMerchantInfo(merchantData)
+        setStep(2)
+      }
+    } catch (error) {
+      
     }
   }
 
-  useEffect(() => {
-    
-  }, []);
-  
+  const createDeposit = async ()=> {
+    const depositData = {
+      amount: depositAmout,
+      narration,
+      merchantId: merchantInfo.data._id
+    }
+    try {
+      const result = await createNairaDepositRequest(depositData);
+      console.log(result);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <div className={`fadded-container modal-overlay ${isOpen ? 'open' : ''}`} >
@@ -34,11 +59,13 @@ function Fiatdeposit({ isOpen, onClose}) {
                   value={depositAmout} 
                   onChange={(e) => setDepositAmount(e.target.value)} 
                   />
+                  <label className="">Enter narration</label>
+                  <input type="text"  placeholder='Eneter narration' 
+                  value={narration} 
+                  onChange={(e) => SetNarration(e.target.value)} 
+                  />
                 </div>
-                
-
-                <button onClick={nextStep}>Proceed</button>
-
+                <button onClick={getMerchant}>Proceed</button>
             </div>)}
             {step === 2 && (
               <div className="bank-peer-wrap">
@@ -46,28 +73,29 @@ function Fiatdeposit({ isOpen, onClose}) {
                 <div className="deposit-head-text-wrap">
                 <span><FaLessThan size={15} onClick={()=>{setStep(1)}} /></span><h3>Deposit Amount</h3>
                 </div>
-                <h3>₦ <span>{depositAmout}</span></h3>
+                <h3>₦<span>{depositAmout}</span></h3>
                 <p>Transfer to </p>
                 <div className="bank-details-wrap">
                   <div className="acc-num">
-                  <h4>Account Number:</h4> <span className='copy-text-wrap'><span>2888208283820</span><IoIosCopy /></span>
+                  <h4>Merchant:</h4> <span className='copy-text-wrap'><span>{merchantInfo?.data.nickname}</span><IoIosCopy /></span>
+                  </div>
+                  <div className="acc-num">
+                  <h4>Account Number:</h4> <span className='copy-text-wrap'><span>{merchantInfo?.data.accountNumber}</span><IoIosCopy /></span>
                   </div>
                   <div className="bank-name">
-                  <h4>Bank:</h4> <span className='copy-text-wrap'><span>GT Bank</span><IoIosCopy /></span>
+                  <h4>Bank:</h4> <span className='copy-text-wrap'><span>{merchantInfo?.data.bankName}</span><IoIosCopy /></span>
                   </div>
                   <div className="acc-name">
-                  <h4>Account Name:</h4> <span className='copy-text-wrap'><span>Umune kingsley</span><IoIosCopy /></span>
+                  <h4>Account Name:</h4> <span className='copy-text-wrap'><span>{merchantInfo?.data.accountName}</span><IoIosCopy /></span>
                   </div>
                 </div>
-                <button className="done-tranfer-btn">I Have Paid</button>
+                <button onClick={createDeposit} className="done-tranfer-btn">I have made payment</button>
               </div>
             )}
 
          </div>
         </div>
     </div>
-
-    
   )
 }
 
