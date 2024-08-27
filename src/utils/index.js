95f06c50262100def505893c170ef33fc677a3ef
Adesojisouljay <adesojiisrael@gmail.com>
2024-08-27 15:36:48 +0100

    navba fixes

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,13 +2,24 @@ export const formatNumbers = (n)=> {
     if(n) return n?.toFixed(3)
     };
 
-export const debounce = (func, delay) => {
-    let timeout;
-    return (...args) => {
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    };
-  };
-  
+    export const formatDate = (isoDate) => {
+      const date = new Date(isoDate);
+      return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    }
+    
+
+    export const formatString = (str) => {
+      if (str.length <= 8) {
+        return str;
+      }
+    
+      const firstPart = str.slice(0, 4);
+      const lastPart = str.slice(-4);
+    
+      return `${firstPart}...${lastPart}`;
+    }
+    
