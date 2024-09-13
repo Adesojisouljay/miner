c27ca2fc266909e8519ce0bc1b6b39bc4d958bb4
kesolink <kingsleyumune14@gmail.com>
2024-09-13 21:49:11 +0100

    new commmit on dashboard refactor

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { FaRegEye } from "react-icons/fa";
+import { FaGift, FaRegEye } from "react-icons/fa";
 import { FaRegCircleQuestion } from "react-icons/fa6";
-import { FaGift } from "react-icons/fa6";
+// import { FaGift } from "react-icons/fa6";
 import { FaRegCopyright } from "react-icons/fa";
 import { DepositHiveModal } from "../components/modal/DepositHive";
-import { fetchTransactionHistory } from "../api/transaction";
+// import { fetchTransactionHistory } from "../api/transaction";
 import "./dashboard.scss";
 import { WithdrawalModal } from "../components/modal/WithdrawalModal";
 import Fiatdeposit from "../components/modal/Fiatdeposit";
@@ -15,6 +15,14 @@ import { FiatWithdrawalModal } from "../components/modal/FiatWithdrawal";
 import { setCurrency } from "../redux/currencySlice";
 import { usdPrice } from "../utils";
 import { ListedTokens } from "../components/listed-tokens/ListedTokens";
+import { TbTransferIn, TbTransferOut } from "react-icons/tb";
+import { HiCircleStack } from "react-icons/hi2";
+import hive from "../assets/hive-logo.png"
+import { TransactionHistory } from "../components/transaction-history/TransactionHistory";
+import { FiSearch } from "react-icons/fi";
+import DBTransctionHistory from "../components/transaction-history/DBTransctionHistory";
+import usdt from "../assets/usdt.svg"
+import usdc from "../assets/usdc.svg"
 
 export const Dashboard = () => {
   const user = useSelector((state) => state.ekzaUser.user);
@@ -32,12 +40,24 @@ export const Dashboard = () => {
   const [buySellOpen, setBuySellOpen] = useState(false);
   const [transactionType, setTransactionType] = useState('buy');
   const [showBalance, setShowBalance] = useState(false);
+  const [activeTab, setActiveTab] = useState('coin-price'); // Default to 'coin-price'
+  const [searchQueryCoinPrice, setSearchQueryCoinPrice] = useState('');
+  const [showMore, setShowMore] = useState(false)
+
+    
 
   useEffect(() => {
     if (selectedCurrency) {
       document.getElementById('currencySelect').value = selectedCurrency;
     }
   }, [selectedCurrency]);
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+};
+  
+ const handleShowMore = () =>{
+  setShowMore(!showMore)
+ }
 
   const actionToggle = () => {
     setAction(!action);
@@ -109,53 +129,230 @@ export const Dashboard = () => {
   return (
     <div className="dashboard-container" onClick={actionToggleClose}>
       <div className="dashboard-content">
-        <div className="dashboard-balance">
-          <div className="dashboard-balance-left">
-            <div className="dashboard-balance-info">
-              <div className="dashboard-currency-select">
-                <select
-                  name=""
-                  id="currencySelect"
-                  onChange={(e) => handleCurrencyChange(e.target.value)}
-                >
-                  <option value="NGN">NGN</option>
+        <div className={`dashboard-content-wrap `}>
+          <div className="dashboard-main border-transparent">
+            <div className="bal-top-wrap">
+              <div className="total-fait-wrap">
+               <div className="bal-text-select-wrap">
+                <p>Total Assets</p> 
+                <select className="change-currency" name="" id="currencySelect" onChange={(e) => handleCurrencyChange(e.target.value)}>
+                  <option className="currencySelect"  value="NGN">NGN</option>
                   <option value="USD">USD</option>
                 </select>
-              </div>
-              <div className="dashboard-balance-amount">
-                <h3>Total balance:</h3>
-                {showBalance ? <h3>********</h3> : <h2>
+               </div>
+                <div className="bal-show-wrap">{showBalance ? <h3>********</h3> : <span>
                   <span className="dashboard-currency-symbol">{isUsd ? "$" : "N"}</span>
                   {isUsd ? (user?.nairaBalance / usdPrice)?.toFixed(3) : user?.nairaBalance.toFixed(3)}
-                </h2>}
+                </span>}</div>
+              </div>
+              <div className="bal-action-wrap">
+               <FaRegEye className="show-balance" onClick={toggleBalanceView} />
+                {/* <div className="bal-action-main-wrap">
+                <div className="bal-deposit-wrap" onClick={() => openDepositModal(assets[0])}>
+                 <TbTransferIn /> <span>Deposit</span>
+                </div>
+                <div className="bal-withdraw-wrap" onClick={openWithdrawalModal}>
+                 <TbTransferOut />  <span>Withdraw</span>
+                </div>
+                </div> */}
               </div>
             </div>
-            <FaRegEye onClick={toggleBalanceView} />
+            <div className="bal-btn-wrap-main">
+            <div className={`show-more-wrap ${!showMore ? "margin-bottom-large" : "margin-bottom-small"}`}><span className="show-more" onClick={handleShowMore }>{!showMore ? "Show more" : "Show less"}</span></div>
+            <div className="bal-btn-wrap">
+              <btn className="bal-btn">
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Buy Assets</span>
+              </btn>
+              <btn className="bal-btn">
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Sell Assets</span>
+              </btn>
+              <btn className="bal-btn" onClick={openFiatTransferModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Fiat Transfer</span>
+              </btn>
+              <btn className="bal-btn" onClick={openFiatDepositModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Fiat Deposit</span>
+              </btn>
+              <btn className={`bal-btn-less ${!showMore ? "display-none" : "display-block"}`} onClick={openFiatWithdrawalModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Fiat Withdrawal</span>
+              </btn>
+              <btn className={`bal-btn-less ${!showMore ? "display-none" : "display-block"}`} onClick={openFiatWithdrawalModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Fiat Withdrawal</span>
+              </btn>
+              <btn className={`bal-btn-less ${!showMore ? "display-none" : "display-block"}`} onClick={openFiatWithdrawalModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Fiat Withdrawal</span>
+              </btn>
+              <btn className={`bal-btn-less ${!showMore ? "display-none" : "display-block"}`} onClick={openFiatWithdrawalModal}>
+                <div className="bal-icon-wrap">
+                <HiCircleStack />
+                </div>
+                <span>Swap</span>
+              </btn>
+              
+            </div>
+            </div>
+
           </div>
-          <div className="dashboard-balance-right">
-            <h4>Bronze Membership</h4>
-            <FaRegCircleQuestion />
-            <FaRegEye />
+          <div className="portfolio-reward-wraper">
+          <div className="card-wrap border-transparent">
+            <div className="card-title-wrap">
+              <div className="card-icon">
+                <FaGift size={20} />
+              </div>
+              <h4>Assets</h4>
+            </div>
+
+            {/* <div className="card-bal">
+              <h2>${user.totalUsdValue.toFixed(3)}</h2>
+              <h2>
+                <span className="strike-naira">N</span>
+                {user.totalNairaValue.toFixed(3)}
+              </h2>
+            </div> */}
+          
+            <div className="card-component-wrap">
+                <div className="  card-component-1 border-line">
+                  <div className="coin-wrap">
+                    <img src={hive} alt="" />
+                    <div className="">
+                      <h5>Hive</h5>
+                      {/* <p>30</p> */}
+                    </div>
+                  </div>
+                  <div> <span>N0.00</span> </div>
+                  <span>0.00()</span>
+                  <div className="btn-deposit-withdrwal">
+                    <button>Withdraw</button>
+                    <button>Buy/Sell</button>
+                  </div>
+                </div>
+                <div className="  card-component-1 border-line">
+                  <div className="coin-wrap">
+                    <img src={usdt} alt="" />
+                    <div className="">
+                      <h5>Usdt</h5>
+                      {/* <p>30</p> */}
+                    </div>
+                  </div>
+                  <div> <span>N0.00</span> </div>
+                  <span>0.00()</span>
+                  <div className="btn-deposit-withdrwal">
+                    <button>Withdraw</button>
+                    <button>Buy/Sell</button>
+                  </div>
+                </div>
+                <div className="  card-component-1 border-line">
+                  <div className="coin-wrap">
+                    <img src={usdc} alt="" />
+                    <div className="">
+                      <h5>Usdc</h5>
+                      {/* <p>30</p> */}
+                    </div>
+                  </div>
+                  <div> <span>N0.00</span> </div>
+                  <span>0.00()</span>
+                  <div className="btn-deposit-withdrwal">
+                    <button>Withdraw</button>
+                    <button>Buy/Sell</button>
+                  </div>
+                </div>
+                <div className={` ${!showMore ? "display-none":"card-component-1 border-line"}`}>
+                  <div className="coin-wrap">
+                    <img src={usdc} alt="" />
+                    <div className="">
+                      <h5>Usdc</h5>
+                      {/* <p>30</p> */}
+                    </div>
+                  </div>
+                  <div> <span>N0.00</span> </div>
+                  <span>0.00()</span>
+                  <div className="btn-deposit-withdrwal">
+                    <button>Withdraw</button>
+                    <button>Buy/Sell</button>
+                  </div>
+                </div>
+
+
+              {/* ))} */}
+            </div>
           </div>
         </div>
-        <div className="dashboard-funding">
-          <button onClick={() => openDepositModal(assets[0])}>Receive</button>
-          <button onClick={openWithdrawalModal}>Send</button>
-          <button onClick={openFiatTransferModal}>Fiat Transfer</button>
-          <button onClick={openFiatDepositModal}>Fiat Deposit</button>
-          <button onClick={openFiatWithdrawalModal}>Fiat Withdrawal</button>
-          <button className="dashboard-buy-sell-btn" onClick={() => openBuySellModal('buy')}>
-            Buy/Sell
-          </button>
-          {action &&
-            <div className="dashboard-action-buttons">
-              <h3 onClick={actionToggle}>Buy</h3>
-              <h3 onClick={actionToggle}>Sell</h3>
-            </div>}
         </div>
-        <div>
-          <ListedTokens />
+<div className="bal-big-container-wrap">
+    <div className="tabs-wrap">
+        <div className="left-tabs-wrap">
+            <div
+                className={`coin-price ${activeTab === 'coin-price' ? 'activetab' : ''}`}
+                onClick={() => handleTabClick('coin-price')}
+            >
+                Coin Price
+            </div>
+            <div
+                className={`transaction ${activeTab === 'transaction' ? 'activetab' : ''}`}
+                onClick={() => handleTabClick('transaction')}
+            >
+                Transaction
+            </div>
+            <div
+                className={`staked-asset ${activeTab === 'stake' ? 'activetab' : ''}`}
+                onClick={() => handleTabClick('stake')}
+            >
+                Stake Asset
+            </div>
         </div>
+
+        <div className="right-tabs-wrap">
+            <input className={`search-bal-input ${activeTab === 'transaction' ? 'activetab' : ''}`} type="text" placeholder="Search..." />
+            <input className={`search-coin-price ${activeTab === 'coin-price' ? 'activetab' : ''}`} type="text" placeholder=" name or ID..."  value={searchQueryCoinPrice}
+                onChange={(e) => setSearchQueryCoinPrice(e.target.value)}/>
+            <FiSearch size={19} />
+        </div>
+    </div>
+
+    <div className="display-area">
+        <div className={`coin-price-component ${activeTab === 'coin-price' ? 'activetab' : ''}`}>
+            <ListedTokens setSearchQuery={setSearchQueryCoinPrice} searchQuery={searchQueryCoinPrice}  />
+        </div>
+        <div className={`transction-component ${activeTab === 'transaction' ? 'activetab' : ''}`}>
+            <DBTransctionHistory  />
+        </div>
+        <div className={`stake-component ${activeTab === 'stake' ? 'activetab' : ''}`}>
+            <h1>Staking is coming soon</h1>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+        
         <div className="dashboard-footer">
           <FaRegCopyright />
           <p>Adesojisouljay, All Rights Reserved</p>
