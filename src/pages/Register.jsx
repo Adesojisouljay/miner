d52d228c3e8a8ff3dfecae8962164e14af9c13fc
kesolink <kingsleyumune14@gmail.com>
2024-09-24 08:15:57 +0100

    new commit on sign in and register page with adjustment on ui main height

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { registerUser } from '../api/auth';
 import cat from "../assets/document_shape.webp";
 import eth from "../assets/eth-icon.webp";
-import './register.css';
+import './register.scss';
+import Logo from "../assets/download (1).png";
 
 const Register = () => {
   const [email, setEmail] = useState('');
@@ -41,12 +42,92 @@ const Register = () => {
   };
 
   return (
-    <div className="reg-wrap">
-      <img className="img-1" src={eth} alt="ETH Icon" />
-      <img className="img-2" src={cat} alt="Document Shape" />
-      <div className="register-container">
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit}>
+    // <div className="reg-wrap">
+    //   <img className="img-1" src={eth} alt="ETH Icon" />
+    //   <img className="img-2" src={cat} alt="Document Shape" />
+    //   <div className="register-container">
+    //     <h2>Register</h2>
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="reg-form-group">
+    //         <label>First Name</label>
+    //         <input
+    //           type="text"
+    //           value={firstName}
+    //           onChange={(e) => setFirstName(e.target.value)}
+    //           placeholder="Enter your first name"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="reg-form-group">
+    //         <label>Last Name</label>
+    //         <input
+    //           type="text"
+    //           value={lastName}
+    //           onChange={(e) => setLastname(e.target.value)}
+    //           placeholder="Enter your last name"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="reg-form-group">
+    //         <label>Email</label>
+    //         <input
+    //           type="email"
+    //           value={email}
+    //           onChange={(e) => setEmail(e.target.value)}
+    //           placeholder="Enter your email"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="reg-form-group">
+    //         <label>Username</label>
+    //         <input
+    //           className="yep"
+    //           type="text"
+    //           value={username}
+    //           onChange={(e) => setUsername(e.target.value)}
+    //           placeholder="Enter your wallet address"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="reg-form-group">
+    //         <label>Password</label>
+    //         <input
+    //           className="yep"
+    //           type="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //           placeholder="Enter your password"
+    //           required
+    //         />
+    //       </div>
+    //       <div className="reg-form-group">
+    //         <label>Confirm Password</label>
+    //         <input
+    //           className="test"
+    //           type="password"
+    //           value={confirmPassword}
+    //           onChange={(e) => setConfirmPassword(e.target.value)}
+    //           placeholder="Confirm your password"
+    //           required
+    //         />
+    //       </div>
+    //       {error && <span>{error}</span>}
+    //       <button className="reg-btn" type="submit">Register</button>
+    //     </form>
+    //     <div className="reg-link">
+    //       <span>Already have an account? <Link className="login-span" to="/login">Login</Link></span>
+    //     </div>
+    //   </div>
+    // </div>
+
+
+// *****************new *******************************
+
+
+    <div className="register-container">
+      <div className="reg-left">
+      <h1 className="header-text">Register to Ekza</h1>
+      <form onSubmit={handleSubmit}>
           <div className="reg-form-group">
             <label>First Name</label>
             <input
@@ -111,11 +192,14 @@ const Register = () => {
             />
           </div>
           {error && <span>{error}</span>}
-          <button className="reg-btn" type="submit">Register</button>
+          <button className="btn-register" type="submit">Register</button>
         </form>
         <div className="reg-link">
-          <span>Already have an account? <Link className="login-span" to="/login">Login</Link></span>
+           <span>Already have an account? <Link className="login-span" to="/login">Login</Link></span>
         </div>
+      </div>
+      <div className="reg-right">
+      <img src={Logo} alt="" />
       </div>
     </div>
   );
